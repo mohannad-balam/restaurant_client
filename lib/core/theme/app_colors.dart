7573fbb3ee827f4981b9305102df755b0922002d
mohannad balam <b.mohanned85@gmail.com>
2025-02-18 01:00:45 +0200

    swith main colors between admin and user apps

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -3,32 +3,33 @@ import 'package:flutter/material.dart';
 import '../constant/specific/specific_constants.dart';
 
 class AppColors {
-static const Color mainColor = SpecificConstants.mainColor;
-static const Color secondColor = SpecificConstants.secondColor;
-static const Color thirdColor = SpecificConstants.thirdColor;
-static const Color splashColor = SpecificConstants.splashColor;
-static const Color warningColor = Color(0xFFcc3c38);
-static const Color successColor = Color(0xFF3dc381);
+  static const Color mainColor = SpecificConstants.mainColor;
+  static const Color secondColor = SpecificConstants.secondColor;
+  static const Color thirdColor = SpecificConstants.thirdColor;
+  static const Color splashColor = SpecificConstants.splashColor;
+  static const Color warningColor = Color(0xFFcc3c38);
+  static const Color successColor = Color(0xFF3dc381);
 
 // ----------------------------------- BACKGROUND LIGHT MODE----------------------------------------
 
-static const Color lightBackroundScreenColor = Color(0xFFf8f9ff);
-static const Color mainTextColor = Color(0xFF2a2d30);
-static const Color secondeyTextColor = Color(0xFFebf2f8);
-static const Color greyTextColor = Color.fromARGB(255, 135, 133, 137);
-static const Color greyColor = Color.fromARGB(72, 193, 189, 193);
-static const Color shimmerLightColor = Color.fromARGB(73, 158, 155, 158);
+  static const Color lightBackroundScreenColor = Color(0xFFf8f9ff);
+  static const Color mainTextColor = Color(0xFF2a2d30);
+  static const Color secondeyTextColor = Color(0xFFebf2f8);
+  static const Color greyTextColor = Color.fromARGB(255, 135, 133, 137);
+  static const Color greyColor = Color.fromARGB(72, 193, 189, 193);
+  static const Color shimmerLightColor = Color.fromARGB(73, 158, 155, 158);
 
 // .fromARGB(255, 135, 133, 137);
 // Color(0xFFE2DFD2);//fromARGB(74, 181, 178, 181);
 
 // ----------------------------------- BACKGROUND Dark MODE----------------------------------------
 
-static const Color darkBackroundScreenColor = Color(0xFF121212);
-static const Color darkBackroundContinarColor = Color.fromARGB(255, 45, 44, 44);
-static const Color darkBackroundBottomNavigationBarColor =
-    Color.fromARGB(255, 14, 14, 14);
-static const Color darktitleColor = Color.fromARGB(208, 255, 255, 255);
-static const Color darkMainTextColor = Color(0x99FFFFFF);
-static const Color darksecondeyTextColor = Color(0x4DFFFFFF);
+  static const Color darkBackroundScreenColor = Color(0xFF121212);
+  static const Color darkBackroundContinarColor =
+      Color.fromARGB(255, 45, 44, 44);
+  static const Color darkBackroundBottomNavigationBarColor =
+      Color.fromARGB(255, 14, 14, 14);
+  static const Color darktitleColor = Color.fromARGB(208, 255, 255, 255);
+  static const Color darkMainTextColor = Color(0x99FFFFFF);
+  static const Color darksecondeyTextColor = Color(0x4DFFFFFF);
 }
