921410e1cc1b7e3f176d31b58be89a8fd07a35cd
mohannad balam <b.mohanned85@gmail.com>
2025-02-18 01:15:29 +0200

    switching main color between admin and user

--- lib/core/constant/specific/specific_constants.dart
+++ lib/core/constant/specific/specific_constants.dart
@@ -4,7 +4,7 @@ class SpecificConstants {
   static const String baseUrl = 'http://192.168.0.66:8000/api';
   static const String categoryUrl = 'http://192.168.0.66:8000/categories';
   static const String menuUrl = 'http://192.168.0.66:8000/menus';
-  static const Color mainColor = Color(0xFF295abb);
+  static const Color mainColor = Color(0xFFe74c3c);
   static const Color secondColor = Color(0xFF6ab0f9);
   static const Color thirdColor = Color(0xFFd1e4ff);
   static const Color splashColor = Color(0xFFF8574D);
