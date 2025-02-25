d207a1cef4f7d75cf696dac774b6cf38c68253ba
mohannad balam <b.mohanned85@gmail.com>
2025-02-25 09:24:14 +0200

    url

--- lib/core/constant/specific/specific_constants.dart
+++ lib/core/constant/specific/specific_constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class SpecificConstants {
-  static const String baseUrl = 'http://192.168.0.66:8000/api';
-  static const String categoryUrl = 'http://192.168.0.66:8000/categories';
-  static const String menuUrl = 'http://192.168.0.66:8000/menus';
+  static const String baseUrl = 'http://192.168.39.252:8000/api';
+  static const String categoryUrl = 'http://192.168.39.252:8000/categories';
+  static const String menuUrl = 'http://192.168.39.252:8000/menus';
   static const Color mainColor = Color(0xFFe74c3c);
   static const Color secondColor = Color(0xFF6ab0f9);
   static const Color thirdColor = Color(0xFFd1e4ff);
